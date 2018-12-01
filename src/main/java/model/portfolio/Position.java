88a5711e41237d4a459430b25c1d675b30e730d6
Oleh Kapustkin <oleh.kapustkin@valtech.de>
2018-12-01 18:37:58 +0100

    add build/deploy script refactor possition parsing

--- src/main/java/model/portfolio/Position.java
+++ src/main/java/model/portfolio/Position.java
@@ -7,34 +7,47 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 public class Position {
-    String name;
     String info;
+    String type;
+    String name;
     BigDecimal ammount;
     BigDecimal units;
     BigDecimal open;
     LocalDateTime openTime;
     BigDecimal close;
     LocalDateTime closeTime;
+    LocalDateTime lastUpdated;
     BigDecimal sl;
     BigDecimal tp;
     BigDecimal profitPr;
     BigDecimal profit$;
 
-    public Position(String s, String s1) {
-        this.name = s;
-        this.info = s1;
+    public Position(String s) {
+        this.info = s;
+        parseInfo(s);
+    }
+
+    private void parseInfo(String info) {
+        String arr[] = info.split("\n");
+        this.type = arr[0].split(" ")[0];
+        this.name = arr[0].split(" ")[1];
+        String posInfo[] = arr[2].split(" ");
+        this.lastUpdated = LocalDateTime.parse(arr[1], DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
+        this.info = arr[2];
+
     }
 
     @Override
     public String toString() {
 
-        return String.format("------- %s %s ------\n", name, info);
+        return String.format("------- %s %s %s ------\n", name, type, info);
     }
 }
 
