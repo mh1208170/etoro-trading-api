99c75372f665f6bb440b375531da5b6e6c8d60c3
Oleh Kapustkin <oleh.kapustkin@valtech.de>
2019-04-09 21:35:08 +0200

    add PositionError getters and setters

--- src/main/java/learn/monitoring/PositionError.java
+++ src/main/java/learn/monitoring/PositionError.java
@@ -1,8 +1,17 @@
 package learn.monitoring;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class PositionError {
+
     String id;
     AbstractPosition position;
+
 
     public PositionError(String id, AbstractPosition position) {
         this.id = id;
